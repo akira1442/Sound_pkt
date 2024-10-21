cb216a3a81c80528508d45966c38e0027b4714f6
akira1442 <nawad.karihila@etu.sorbonne.universite.fr>
2024-10-21 15:45:29 +0200

    Ajout des première versions des methodes supp et add Projet

--- ListeProjet.java
+++ ListeProjet.java
@@ -28,13 +28,7 @@ public class ListeProjet {
 
     // Methodes
 
-    public void addProjet(Projet p){
-
-        this.ListProjet.add(p);
-        System.out.println("Project: %s added".formatted(p.toString()));
-    }
-
-    public Projet getElement(String nomAlbum, String nomArtiste){
+    public Projet getProjet(String nomAlbum, String nomArtiste){
 
         for (Projet e : this.ListProjet){
             if ((e.getNom() == nomAlbum) && (e.getArtiste() == nomArtiste)){
@@ -44,6 +38,74 @@ public class ListeProjet {
 
         System.out.println("Album not in lists");
         return null;
+    }
+
+    public Projet getProjet(int index){
+
+        return this.ListProjet.get(index);
+    }
+
+    public void addProjetHead(Projet p){
+
+        /*Ajout en tête de la liste*/
+
+        this.ListProjet.add(p);
+        nbProjet++;
+        System.out.println("Project: %s added".formatted(p.toString()));
+    }
+
+    public void suppProjetHead(Projet p){
+
+        /*Supprime en tête de la liste*/
+
+        if (this.ListProjet.remove(p)){
+            nbProjet--;
+            System.out.println("Project: %s deleted".formatted(p.toString()));
+            return;
+        }
+        System.out.println("Project: %s not in list".formatted(p.toString()));
+    }
+
+    public void addProjetTail(Projet p){
+
+        /*Ajout en queue de la liste*/
+
+        this.ListProjet.add(nbProjet,p);
+        nbProjet++;
+        System.out.println("Project: %s added".formatted(p.toString()));
+    }
+
+    public void suppProjetTail(Projet p){
+
+        /*Supprime en queue de la liste*/
+
+        if (this.ListProjet.remove(p)){
+            nbProjet--;
+            System.out.println("Project: %s deleted".formatted(p.toString()));
+        }
+        System.out.println("Project: %s not in list".formatted(p.toString()));
+    }
+
+    public void addProjetIndex(Projet p, int i){
+
+        /*Ajout le projet à l'indice i*/
+
+        this.ListProjet.add(i,p);
+        nbProjet++;
+        System.out.println("Project: %s added".formatted(p.toString()));
+    }
+
+    public void suppProjetIndex(int i){
+
+        /*Supprime le projet à l'indice i*/
+
+        Projet supp = this.ListProjet.get(i);
+
+        if (this.ListProjet.remove(supp)){
+            nbProjet--;
+            System.out.println("Project: deleted");
+        }
+        System.out.println("Project not in list");
     }
 
     // Accesseurs
